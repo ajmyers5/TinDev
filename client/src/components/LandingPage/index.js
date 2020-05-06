9f2e24dcdb7fb078fefaedce14b750a27642ae9e
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-05-05 19:33:45 -0500

    Adding CRA files. Did some cleaning as well.

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>*** Insert the Components Here ***</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
