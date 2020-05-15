0ae275e73c3e113511d75e1ade67ac94598cdc7d
Andrew Myers <ajmyers5@gmail.com>
2020-05-14 19:07:00 -0500

    add info to GHP card component

--- client/src/components/GHP-Card/index.js
+++ client/src/components/GHP-Card/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function GHPCard() {
+function GHPCard(props) {
   return (
     <div className="container">
       <div className="card mb-4 shadow-sm">
         <div className="card-header">
-          <h4 className="my-0 font-weight-normal">Repositories</h4>
+          <h4 className="my-0 font-weight-normal">{props.header}</h4>
         </div>
         <div className="card-body">
-          <h1 className="card-title">25</h1>
+          <h1 className="card-title">{props.title}</h1>
         </div>
       </div>
     </div>
--- client/src/pages/GithubPage.js
+++ client/src/pages/GithubPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios"
 import Navbar from "../components/Navbar";
 import GHPJumbotron from "../components/GHP-Jumbotron";
-// import GHPCard from "../components/GHP-Card";
+import GHPCard from "../components/GHP-Card";
 
 class GithubPage extends Component {
   state = {
@@ -26,7 +26,9 @@ class GithubPage extends Component {
       <div>
         <Navbar></Navbar>
         <GHPJumbotron name={this.state.user.name} user={this.state.user.user} location={this.state.user.locationOfUser} avatar={this.state.user.avatar}></GHPJumbotron>
-        <h1>{this.props.match.params.id}</h1>
+        <GHPCard header="Number of Followers" title={this.state.user.numFollowers}/>
+        <GHPCard header="Number of Following" title={this.state.user.numFollowing}/>
+        <GHPCard header="Number of Repositories" title={this.state.user.numRepos}/>
       </div>
     );
   }
